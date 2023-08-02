bb80e37de483ad222bed51ef76c77078884cf3e2
thesammy2010 <43960199+thesammy2010@users.noreply.github.com>
2023-08-02 20:23:20 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# TheSammy2010
+
+> source code for https://thesammy2010.com
+
+Also an excuse to learn React properly
+
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "./App.css"
+
+export default function App() {
+    return (
+        <div id="page-container">
+            <div id="content-wrap">
+                <h1 className="title">TheSammy2010</h1>
+            </div>
+            <footer id="footer">
+                <p>
+                    <a href="https://github.com/thesammy2010/thesammy2010.com">©2023 thesammy2010.com</a>
+                </p>
+            </footer>
+        </div>
+    )
+}
