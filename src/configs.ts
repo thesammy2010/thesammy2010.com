c8d906150b64e27bb9250f6db321fb68a1299f4e
Leo <lparkesneptune@blockchain.com>
2025-12-13 23:39:53 +0000

    fix(envs): use `REACT_APP_` prefix

--- src/configs.ts
+++ src/configs.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL || "http://localhost:8000"
+export const API_URL = process.env.REACT_APP_API_URL
 
 export function countryCodeToEmoji(countryCode: string): string {
     const codePoints = countryCode
